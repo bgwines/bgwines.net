67c0a0ec5d4ed39095960539f5c126aaa08fb6f7
Brett Wines <brettwines@gmail.com>
2014-07-22 02:00:59 -0700

    Clean-up. Removed logo from redirected pages and made most links on the home page directly to the github pages of the projects.

--- quineify.py
+++ quineify.py
@@ -22,12 +22,16 @@ def whole_file_processed():
 		whole_file += process_line(line)
 	return whole_file
 
+def process_quine_line(quine_line):
+	# TODO
+	return quine_line;
+
 def insert_quine(line):
 	i = line.index(';')
-	return line[0:i] + "\"" + whole_file_processed() + "\"" + line[i:]
+	return process_quine_line(line[0:i] + "\"" + whole_file_processed() + "\"" + line[i:])
 
 def is_quine_line(line):
-	return "file = ;" in line
+	return "file = ;" == line.strip()
 
 def make_quine():
 	quine = ""
